5840c26b0a8a1796b71d859013f94f9f8f331245
Elisabeth Farias <elisabethf@ciandt.com>
2021-10-13 22:38:07 -0300

    Initial commit

---
+++ app/src/main/java/br/com/dio/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.dio.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.dio.myapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/br/com/dio/myapplication/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.dio.myapplication.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
